4a656e375016b284eb4239e3440f565447738dfa
dev04 <anh.pham@solislab.com>
2017-07-28 10:11:29 +0700

    Edit code in Homepage

--- wp-content/themes/twentyseventeen/index.php
+++ wp-content/themes/twentyseventeen/index.php
@@ -36,80 +36,33 @@ get_header(); ?>
 
 
             <?php
-//                add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
-//
-//                function add_my_post_types_to_query( $query ) {
-//                    if ( is_home() && $query->is_main_query())
-//                        $query->set( 'post_type', 'tst_events') );
-//                    return $query;
-//                }
 
-            $args = array(
-                    'post_type' => 'tst_events',
-                    'post_per_page' => 5
-            );
+                $args = array(
+                        'post_type' => 'tst_events',
+                        'posts_per_page' => 5
+                );
 
-            $list = new WP_Query( $args );
-            if ($list->have_posts()):
-            while ($list->have_posts() ) : $list->the_post();
+                $list = new WP_Query( $args );
 
+                if ($list->have_posts() ):
+                while ($list->have_posts() ) : $list->the_post();
             ?>
-                <h2 id="events-<?php the_ID(); ?>"
-                    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">
-                       <?php the_title(); ?>
-                    </a>
-                </h2>
 
-                <?php the_content(ReadMore, FALSE); ?>
-                <?php the_excerpt(); ?>
-
-                <?php endwhile;
-                endif;
-            ?>
+                <h2 id="events-<?php the_ID(); ?>"><?php the_title(); ?></h2>
 
                 <?php
-//            if( $list ) {
-//                echo '<ul>';
-//                foreach( $list as $post ) : setup_postdata($post);
-//                echo '<li>',
-//                        '<h2>'. get_the_title() .'</h2>',
-//                            get_the_content() .
-//                            get_the_excerpt () .
-//                      '</li>';
-//                endforeach;
-//                echo '<ul>';
-//                wp_reset_postdata();
-//            }
+                    the_excerpt();
+                    endwhile;
+                    the_post_navigation( array(
+                        'mid_size' => 3,
+                        'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
+                        'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
+                    ) );
 
-//            if ( have_posts() ) :
-//
-//                /* Start the Loop */
-//                while ( have_posts() ) : the_post();
-//
-//                    /*
-//                     * Include the Post-Format-specific template for the content.
-//                     * If you want to override this in a child theme, then include a file
-//                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-//                     */
-//                    get_template_part( 'template-parts/post/content', get_post_format() );
-//
-//                endwhile;
-//
-//                the_posts_pagination( array(
-//                    'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-//                    'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-//                    'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-//                ) );
-//
-//            else :
-//
-//                get_template_part( 'template-parts/post/content', 'none' );
-//
-//            endif;
+                     wp_reset_postdata();
 
-
-            ?>
-
+                    endif;
+                ?>
 
 
 
